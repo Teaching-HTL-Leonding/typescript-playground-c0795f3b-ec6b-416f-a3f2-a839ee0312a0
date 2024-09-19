0e54fae20fbb71c559ddcdf775842f77938c7e32
Rainer Stropek <rainer@timecockpit.com>
2024-09-19 08:57:28 +0200

    Saved in TypeScript Playground at 2024-09-19T06:57:27.911Z

--- olympic_rings/index.ts
+++ olympic_rings/index.ts
@@ -2,18 +2,30 @@ function setup() {
 createCanvas(800, 900);
 background("white");
 noFill();
-
-  strokeWeight(7);  
-  circle(200, 150, 175)
+  
+  strokeWeight(5);  
   stroke("blue");
+  circle(200, 150, 175)
+  
 
-  circle(380, 150, 175)
+  strokeWeight(5);  
   stroke("black");
+  circle(385, 150, 175)
+  
 
-  circle(560, 150, 175)
-  stroke
+  strokeWeight(5);  
+  stroke("red");
+  circle(570, 150, 175)
+
+  strokeWeight(5); 
+  stroke("orange"); 
   circle(290, 280, 175)
-  circle(470, 280, 175)
+  
+
+  strokeWeight(5);  
+  stroke("green");
+  circle(480, 280, 175)
+
 
 
 
