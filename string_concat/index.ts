f34abd9df6318c2fcb542b5de4ab86a5dc61d79e
Rainer Stropek <rainer@timecockpit.com>
2024-10-01 18:52:20 +0200

    Saved in TypeScript Playground at 2024-10-01T16:52:20.195Z

--- string_concat/index.ts
+++ string_concat/index.ts
@@ -1,25 +1,25 @@
-let mouseProtocol: string = "0/0";
-
-
+let mouseProtocol: string = `0/0`;
 
 function setup() {
   createCanvas(400, 400);
   background("skyblue");
-  textSize(10);
-  text(mouseProtocol, 10, 10, 360, 360)
+  textSize(20);
+  text(mouseProtocol, 20, 20, 360, 360)
 }
 
 
 function mouseClicked() {
 background("skyblue");
+ mouseProtocol += `, ${mouseX}/${mouseY}`;
+
 
   noStroke();
   fill("magenta");
   circle(mouseX, mouseY, 25);
 
-  mouseProtocol: `${mouseProtocol} , ${mouseX}, ${mouseY}`;
+
   fill("black");
-  text(mouseProtocol, 10, 10, 360, 360 );
+  text(mouseProtocol, 20, 20, 360, 360 );
 }
 
 
