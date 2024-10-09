17ad75c34b24794e834cbe4cd56915c36119fc2e
Rainer Stropek <rainer@timecockpit.com>
2024-10-09 19:38:01 +0200

    Saved in TypeScript Playground at 2024-10-09T17:38:01.512Z

--- color_areas/index.ts
+++ color_areas/index.ts
@@ -3,7 +3,7 @@ function setup() {
     background("black");
     noStroke();
 
-      fill("yellow");
+    fill("yellow");
     rect(0, 0, width / 3);
 
     fill("red");
@@ -12,25 +12,34 @@ function setup() {
     fill("green");
     rect(width / 3 * 2, 0, width / 3);
 }
-    function mouseClicked() {
+
+function mouseClicked() {
 
     fill("black");
     rect(0, 150, width, 50);
 
-    fill("white");}
+    fill("white");
+    // The message variable will recive the message to print
+    let message: string;
 
-     if (mouseX < width / 3) {
+    // Note the ìf statement here. It checks is the mouse click
+    // was in the area (yellow). If it was, the message is "Yellow".
+    if (mouseX < width / 3) {
         message = "Yellow"
-         } else if (mouseX < width / 3 * 2) {
+    } else if (mouseX < width / 3 * 2) {
         message = "Red";
-        
-        message = "Green";}
-
-
-
+        // Note the else if statement here. If the user did not 
+        // click in the yellow area, it checks if the user clicked 
+        // in  the second area (green).
+    } else {
+        message = "Green";
+        // If the click was not in the yellow and not in the green
+        // area, the click must have been in the area.
+    }
     textAlign(CENTER);
     textSize(40);
     text(message, 200, 180);
+}
 
 
 
