0b255aee983eb488d000edcb19d13270bdccf009
Rainer Stropek <rainer@timecockpit.com>
2024-10-09 19:41:24 +0200

    Saved in TypeScript Playground at 2024-10-09T17:41:23.899Z

--- color_quadrants/index.ts
+++ color_quadrants/index.ts
@@ -3,8 +3,12 @@ function setup() {
     background("black");
     noStroke();
 
-    fill("yellow");
+      fill("yellow");
     rect(0, 0, width / 2, height / 2);
+
+
+    fill("red");
+    rect(width / 2, 0, width / 2, height / 2);
 
 
    
