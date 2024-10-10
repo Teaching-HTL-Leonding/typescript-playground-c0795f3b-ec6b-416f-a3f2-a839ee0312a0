65d90d1e8262a351016d5587c79a1861c54e872c
Rainer Stropek <rainer@timecockpit.com>
2024-10-10 10:26:29 +0200

    Saved in TypeScript Playground at 2024-10-10T08:26:28.888Z

--- digits/index.ts
+++ digits/index.ts
@@ -25,6 +25,26 @@ const num = Math.floor(random(0, 1_000_000));
   noStroke();
   text(num, width / 2, height - 20);
 
+ noFill();
+ stroke("yellow"); 
+
+  rect(50, height / 2, 30, 50);
+  rect(100, height / 2, 30, 50);
+  rect(150, height / 2, 30, 50);
+  rect(200, height / 2, 30, 50);
+  rect(250, height / 2, 30, 50);
+  rect(300, height / 2, 30, 50);
+
+   fill("yellow");
+  textSize(30);
+  text(`${lastDigit1}`, 315, height / 2 + 25);
+  text(`${digit2}`, 265, height / 2 + 25);
+  text(`${digit3}`, 215, height / 2 + 25);
+  text(`${digit4}`, 165, height / 2 + 25);
+  text(`${digit5}`, 115, height / 2 + 25);
+  text(`${digit6}`, 65, height / 2 + 25);
+}
+
 
  
 
