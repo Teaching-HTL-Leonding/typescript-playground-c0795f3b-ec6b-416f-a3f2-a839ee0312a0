9f7b5db332a9e4bc12a4174abbcbb120f0eb9897
Rainer Stropek <rainer@timecockpit.com>
2024-10-10 10:15:44 +0200

    Saved in TypeScript Playground at 2024-10-10T08:15:44.328Z

--- digits/index.ts
+++ digits/index.ts
@@ -12,7 +12,18 @@ const num = Math.floor(random(0, 1_000_000));
   const digit3 = numWithoutLastDigit % 10;
   numWithoutLastDigit = Math.floor(numWithoutLastDigit/ 10);
 
- 
+  const digit4 = numWithoutLastDigit % 10;
+  numWithoutLastDigit = Math.floor(numWithoutLastDigit / 10);
+
+  const digit5 = numWithoutLastDigit % 10;
+  numWithoutLastDigit = Math.floor(numWithoutLastDigit / 10);
+
+  const digit6 = numWithoutLastDigit % 10;
+
+ textAlign(CENTER, CENTER);
+  fill("yellow");
+  noStroke();
+  text(num, width / 2, height - 20);
 
 
  
