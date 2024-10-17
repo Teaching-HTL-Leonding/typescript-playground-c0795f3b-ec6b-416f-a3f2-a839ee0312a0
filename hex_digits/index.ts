abd13424c2ed36d44f2c49ec3f93fa195719341d
Rainer Stropek <rainer@timecockpit.com>
2024-10-17 08:13:41 +0200

    Saved in TypeScript Playground at 2024-10-17T06:13:40.782Z

--- hex_digits/index.ts
+++ hex_digits/index.ts
@@ -1,24 +1,24 @@
 function setup() {
 createCanvas(400, 400); 
 background ("black") 
-const num = Math.floor(random(0, 64));
+const num = Math.floor(random(0, 4096));
 
- const lastDigit1 = num % 2
- let numWithoutLastDigit = Math.floor(num / 2);
+ const lastDigit1 = num % 16
+ let numWithoutLastDigit = Math.floor(num / 16);
 
- const digit2 = numWithoutLastDigit % 2;
+ const digit2 = numWithoutLastDigit % 16;
+  numWithoutLastDigit = Math.floor(numWithoutLastDigit / 16);
+
+  const digit3 = numWithoutLastDigit % 16;
+  numWithoutLastDigit = Math.floor(numWithoutLastDigit/ 16);
+
+  const digit4 = numWithoutLastDigit % 16;
+  numWithoutLastDigit = Math.floor(numWithoutLastDigit / 16);
+
+  const digit5 = numWithoutLastDigit % 16;
   numWithoutLastDigit = Math.floor(numWithoutLastDigit / 2);
 
-  const digit3 = numWithoutLastDigit % 2;
-  numWithoutLastDigit = Math.floor(numWithoutLastDigit/ 2);
-
-  const digit4 = numWithoutLastDigit % 2;
-  numWithoutLastDigit = Math.floor(numWithoutLastDigit / 2);
-
-  const digit5 = numWithoutLastDigit % 2;
-  numWithoutLastDigit = Math.floor(numWithoutLastDigit / 2);
-
-  const digit6 = numWithoutLastDigit % 2;
+  const digit6 = numWithoutLastDigit % 16;
 
  textAlign(CENTER, CENTER);
   fill("yellow");
@@ -28,18 +28,4 @@ const num = Math.floor(random(0, 64));
  noFill();
  stroke("yellow"); 
 
-  rect(50, height / 2, 30, 50);
-  rect(100, height / 2, 30, 50);
-  rect(150, height / 2, 30, 50);
- 
-   fill("yellow");
-  textSize(30);
-  text(`${digit4}`, 165, height / 2 + 25);
-  text(`${digit5}`, 115, height / 2 + 25);
-  text(`${digit6}`, 65, height / 2 + 25);
 }
-
-
- 
-
-
