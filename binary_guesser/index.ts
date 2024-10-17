556ea8119937745865e095e7476f9fe23a437d3a
Rainer Stropek <rainer@timecockpit.com>
2024-10-17 10:02:27 +0200

    Saved in TypeScript Playground at 2024-10-17T08:02:27.471Z

--- binary_guesser/index.ts
+++ binary_guesser/index.ts
@@ -1,12 +1,13 @@
 let num = 0;
-let digit2 = 0;
-let digit3 = 0;
 let digit4 = 0;
-let digit5 = 0;
+let digit3 = 0;
+let digit2 = 0;
+let digit1 = 0;
 let digitclick = 0;
+let binarynow= 0;
 
 function setup() {
-  num = Math.floor(random(0, 16));
+  num = Math.floor(random(1, 16));
 
   createCanvas(400, 300);
   background("black");
@@ -25,20 +26,65 @@ function setup() {
 
 
 
-  text(`${digit2}`, 75, height / 2);
+  text(`${digit4}`, 75, height / 2);
   text(`${digit3}`, 155, height / 2);
-  text(`${digit4}`, 235, height / 2);
-  text(`${digit5}`, 315, height / 2);
+  text(`${digit2}`, 235, height / 2);
+  text(`${digit1}`, 315, height / 2);
 
 
 
 
-  function mouseClicked(){
-  if ((mouseX < 100) && (mouseX > 50)) && (mouseY > (height / 2 - 40)) && (mouseY < (height / 2 - 80)){
-  digitclick= 4
-  if(digit4=== 0){
-  digit4= 1
+    if ((mouseX < 100) && (mouseX > 50) && (mouseY > (height / 2 - 40)) && (mouseY < (height / 2 + 80))) {
+    digitclick = 4;
+    if (digit4 === 0) {
+      digit4 = 1;
+    }
+    else {
+      digit4 = 0;
+    }
+    fill("black");
+    rect(50, height / 2 - 40, 50, 80);
+    fill("yellow");
+    text(`${digit4}`, 75, height / 2);
   }
+  if ((mouseX < 180) && (mouseX > 130) && (mouseY > (height / 2 - 40)) && (mouseY < (height / 2 + 80))) {
+    digitclick = 3;
+    if (digit3 === 0) {
+      digit3 = 1;
+    }
+    else {
+      digit3 = 0;
+    }
+    fill("black");
+    rect(130, height / 2 - 40, 50, 80);
+    fill("yellow");
+    text(`${digit3}`, 155, height / 2);
   }
-} 
-} 
+if ((mouseX < 260) && (mouseX > 210) && (mouseY > (height / 2 - 40)) && (mouseY < (height / 2 + 80))) {
+    digitclick = 2;
+    if (digit2 === 0) {
+      digit2 = 1;
+    }
+    else {
+      digit2 = 0;
+    }
+    fill("black");
+    rect(210, height / 2 - 40, 50, 80);
+    fill("yellow");
+    text(`${digit2}`, 235, height / 2);
+  }
+  if ((mouseX < 340) && (mouseX > 290) && (mouseY > (height / 2 - 40)) && (mouseY < (height / 2 + 80))) {
+    digitclick = 1;
+    if (digit1 === 0) {
+      digit1 = 1;
+    }
+    else {
+      digit1 = 0;
+    }
+    fill("black");
+    rect(290, height / 2 - 40, 50, 80);
+    fill("yellow");
+    text(`${digit1}`, 315, height / 2);
+  }
+  
+
