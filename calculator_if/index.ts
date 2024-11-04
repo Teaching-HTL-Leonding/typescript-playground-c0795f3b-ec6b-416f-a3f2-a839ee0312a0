8bb08991d8d47e2c302c6bf8e11c6d30aa40981c
Rainer Stropek <rainer@timecockpit.com>
2024-11-04 08:07:35 +0100

    Saved in TypeScript Playground at 2024-11-04T07:07:35.418Z

---
+++ calculator_if/index.ts
@@ -0,0 +1,21 @@
+const MARGIN_NUM = 10;
+
+let num: number = 0;          // Current number entered by the user
+let lineHeight: number = 0;   // Height of a line of the responsive layout
+let cellWidth: number = 0;    // Width of a cell of the responsive layout
+
+function setup() {
+    createCanvas(300, 600);
+    lineHeight = height / 5;
+    cellWidth = width / 3;
+}
+
+function draw() {
+    background("lightgray");
+
+
+}
+
+function mouseClicked() {
+    // <<< Add code here
+}
