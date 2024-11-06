3cc5ed0bb783b7342391eb41face527798318c79
Rainer Stropek <rainer@timecockpit.com>
2024-11-06 16:41:42 +0100

    Saved in TypeScript Playground at 2024-11-06T15:41:41.560Z

--- rps_part_2/index.ts
+++ rps_part_2/index.ts
@@ -9,29 +9,26 @@ const SCISSORS_LEFT = 300;
 const ICON_WIDTH = 100;
 const ICON_TOP = 75;
 const ICON_HEIGHT = 100;
- 
- 
+
+
+
 function setup() {
     createCanvas(500, 490);
     background("black");
- 
+
     fill("yellow");
     textSize(30);
     text("Human:", TEXT_LEFT, 50);
- 
- 
+
     // Display the icons for "stone", "paper", and "scissors".
     textSize(75);
-    text("🪨", STONE_LEFT, 150);
-    text("📃", PAPER_LEFT, 150);
-    text("✂️", SCISSORS_LEFT, 150);
- 
- 
-    // <<< Add your code here
+    const HUMAN_SYMBOLS_TOP = 150;
+    text("🪨", STONE_LEFT, HUMAN_SYMBOLS_TOP);
+    text("📃", PAPER_LEFT, HUMAN_SYMBOLS_TOP);
+    text("✂️", SCISSORS_LEFT, HUMAN_SYMBOLS_TOP);
 }
- 
+
 function mouseMoved() {
-    // <<< Add your code here
     if (mouseX >= 50 && mouseX <= 50 + ICON_WIDTH && mouseY >= 75 && mouseY <= 50 + ICON_HEIGHT) {
         noFill();
         stroke("yellow");
@@ -117,5 +114,3 @@ function mouseClicked() {
     }
  
 }
-
-
