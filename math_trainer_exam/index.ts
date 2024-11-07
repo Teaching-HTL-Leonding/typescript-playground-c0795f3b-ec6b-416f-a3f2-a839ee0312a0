cfd48e81dde79a6549de9fe1d417b78adcb2536e
Rainer Stropek <rainer@timecockpit.com>
2024-11-07 10:29:18 +0100

    Saved in TypeScript Playground at 2024-11-07T09:29:18.383Z

--- math_trainer_exam/index.ts
+++ math_trainer_exam/index.ts
@@ -21,6 +21,8 @@ let rightAnswer = 0;
 function setup() {
     createCanvas(400, 400);
     background("black");
+    fill("yellow");
+    textSize(40);
 
     randomnum1 = Math.floor(random(1, 101));
     randomnum2 = Math.floor(random(1, 101));
@@ -29,64 +31,93 @@ function setup() {
     randomnum4 = Math.floor(random(1, 101));
     randomnum5 = Math.floor(random(1, 101));
 
+
     dice = Math.floor(random(1, 4)); {
-    } if (dice === 1)
-        textmessage = "+"; {
+    } if (dice === 1) {
+        textmessage = "+";
+        rightAnswer = randomnum1 + randomnum2;
 
-    }else if (dice === 2)
-        textmessage = "-"; {
+    } else if (dice === 2) {
+        textmessage = "-";
+        rightAnswer = randomnum1 - randomnum2;
 
-    }else if (dice === 3) {
+    } else if (dice === 3) {
         textmessage = "*";
+        rightAnswer = randomnum1 * randomnum2;
     }
 
-    fill("yellow");
-    textSize(40);
     text(`${randomnum1} ${textmessage} ${randomnum2}`, width / 2 - 70, 50);
     text(`${randomnum3}`, width / 2 - 150, height / 2);
     text(`${randomnum4}`, width / 2 - 35, height / 2);
     text(`${randomnum5}`, width / 2 + 100, height / 2);
 
+
     const computersymbole = Math.floor(random(0, 3));
     if (computersymbole === 0) {
         randomnum3 = rightAnswer;
-    } else if (computersymbole === 0) {
+    } else if (computersymbole === 1) {
         randomnum4 = rightAnswer;
-    } else if (computersymbole === 0) {
+    } else if (computersymbole === 2) {
         randomnum5 = rightAnswer;
-
-
-
-
-
-
-
-
-
-
-
-
     }
+    const position = Math.floor(random(1, 4));
+    if (position === 1) {
+        text(`${rightAnswer}`, width / 2 - 150, height / 2);
 
-    // mouseClicked function checks if the user clicked on the correct answer
-    function mouseClicked() {
-        // <<< TODO: Add your code here
+    } else if (position === 2) {
+        text(`${rightAnswer}`, width / 2 - 35, height / 2);
+
+    } else if (position === 3) {
+        text(`${rightAnswer}`, width / 2 + 100, height / 2);
+
+       } if (position !== 1) {
+            text(`${randomnum3}`, width / 2 - 150, height / 2);
+
+        } else if (position !== 2) {
+            text(`${randomnum4}`, width / 2 - 35, height / 2);
+
+        } else if (position !== 3) {
+            text(`${randomnum5}`, width / 2 + 100, height / 2 + 0);
+        }
+        
 
 
-    }
 
-    // mouseMoved function displays mouse coordinates as feedback
-    function mouseMoved() {
-        // This is a HELPER FUNCTION. It should make finding coordinates
-        // easier for you. You DO NOT NEED to change this method!
 
-        fill("black");
-        noStroke();
-        rect(0, height - 20, width, height); // Draws a rectangle to clear previous coordinates
 
-        fill("white");
-        textSize(10);
-        textAlign(LEFT, BOTTOM); // Aligns coordinates text to bottom-left
-        text(`${mouseX}/${mouseY}`, 5, height - 5); // Displays current mouse coordinates
-    }
-}
+
+
+
+
+
+
+
+
+        }
+
+        // mouseClicked function checks if the user clicked on the correct answer
+        function mouseClicked() {
+            // <<< TODO: Add your code here
+            if(position === 1 && mouseX > width / 2 - 150 && mouseX > width / 2 - 200 && mouseY )
+            if(position === 2 && mouseX > width / 2 - 35 && mouseX < width / 2 + 85 && mouseY)
+            if (position === 3 && mouseX > width / 2 + 100 && mouseX < width / 2 + 150 && mouseY)
+
+
+
+        }
+
+        // mouseMoved function displays mouse coordinates as feedback
+        function mouseMoved() {
+            // This is a HELPER FUNCTION. It should make finding coordinates
+            // easier for you. You DO NOT NEED to change this method!
+
+            fill("black");
+            noStroke();
+            rect(0, height - 20, width, height); // Draws a rectangle to clear previous coordinates
+
+            fill("white");
+            textSize(10);
+            textAlign(LEFT, BOTTOM); // Aligns coordinates text to bottom-left
+            text(`${mouseX}/${mouseY}`, 5, height - 5); // Displays current mouse coordinates
+        }
+    
