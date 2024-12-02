6c4e358bc13610cdce44910cff22cdd1220a417f
Rainer Stropek <rainer@timecockpit.com>
2024-12-02 15:43:22 +0100

    Saved in TypeScript Playground at 2024-12-02T14:43:21.918Z

--- step_lines/index.ts
+++ step_lines/index.ts
@@ -9,33 +9,35 @@ function setup() {
     stroke("lightgray");
 
     // Draw vertical lines
-    for (let i = GRID; i < SIZE; i += GRID)
-        while (i < SIZE) {
-            line(i, 0, i, SIZE);
-            i += GRID;
-        }
+    let i = GRID;
+    while (i < SIZE) {
+        line(i, 0, i, SIZE);
+        i += GRID;
+    }
 
     // Draw horizontal lines
-    for (let i = GRID; i < SIZE; i += GRID) {
-    line(0, i, SIZE, i);
-
+    i = GRID;
+    while (i < SIZE) {
+        line(0, i, SIZE, i);
+        i += GRID;
     }
 
     strokeWeight(2);
 
     // Draw left part of the lines
-    for (let i = GRID; i < SIZE; i += GRID) {
-        stroke("yellow");
-
-        line(GRID, i, GRID * (i / GRID), i)
-
+    // <<< Write your code here
+      i = GRID;
+    while (i < SIZE) {
+           stroke("yellow");
+        line(GRID, i, GRID * (i / GRID), i);
+        i += GRID;
     }
-
     // Draw right part of the lines
     stroke("red");
     // <<< Write your code here
-    for (let i = GRID; i < SIZE; i += GRID) {
-        line(GRID * (i / GRID), i, SIZE - GRID, i)
-
-    }
+      i = GRID;
+    while (i < SIZE) {
+        line(GRID * (i / GRID), i, SIZE - GRID, i);
+        i += GRID;
+}
 }
