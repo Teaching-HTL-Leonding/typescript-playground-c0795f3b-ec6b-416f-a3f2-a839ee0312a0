3307171a5937d1bb3c3edd981438f0e7411be13b
Rainer Stropek <rainer@timecockpit.com>
2024-12-03 09:41:30 +0100

    Saved in TypeScript Playground at 2024-12-03T08:41:30.241Z

---
+++ christmas_tree/index.ts
@@ -0,0 +1,26 @@
+const TOP_MARGIN = 80;
+const SPHERE_DIAMETER = 30;
+const INITIAL_TRIANGLE_BASE_WIDTH = 100;
+const SIZE_INCREASE_BY_LAYER = 0.5;
+const HEIGHT_TO_WIDTH_RATIO = 0.5;
+const NUMBER_OF_LAYERS = 4;
+const TRUNK_WIDTH = 50;
+const TRUNK_HEIGHT = 50;
+
+function setup() {
+  createCanvas(500, 500);
+  background("black");
+  fill("green");
+  noStroke();
+
+  // <<< Draw the tree
+  let trianglewidth = INITIAL_TRIANGLE_BASE_WIDTH
+  translate(width / 2, TOP_MARGIN);
+  for(let i = 0; i <= NUMBER_OF_LAYERS; i ++){
+    triangle(trianglewidth / 2, trianglewidth / 2, 0, 0, trianglewidth / 2, trianglewidth / 2);
+    translate(0, trianglewidth / 4);
+    trianglewidth *= 1,5;
+    fill("brown");
+
+}
+}
