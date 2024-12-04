43b829db790e8ca585d6f4742f775f3480a0135f
Rainer Stropek <rainer@timecockpit.com>
2024-12-04 09:00:43 +0100

    Saved in TypeScript Playground at 2024-12-04T08:00:43.219Z

--- christmas_tree/index.ts
+++ christmas_tree/index.ts
@@ -1,3 +1,4 @@
+
 const TOP_MARGIN = 80;
 const SPHERE_DIAMETER = 30;
 const INITIAL_TRIANGLE_BASE_WIDTH = 100;
@@ -29,5 +30,9 @@ function setup() {
     trianglewidth *= 1.5;
     colorofcircle += 50;
 
+
 }
+fill("brown");
+  rect(-25, 84, 50, 50);
 }
+
