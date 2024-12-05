aadbf0633e81c31fc29b3a72138d7d16fbc1cf7f
Rainer Stropek <rainer@timecockpit.com>
2024-12-05 09:05:31 +0100

    Saved in TypeScript Playground at 2024-12-05T08:05:31.293Z

--- pattern_with_push_and_pop/index.ts
+++ pattern_with_push_and_pop/index.ts
@@ -1,30 +1,49 @@
 function setup() {
-    const SIZE = 400;
-    const CIRCLE_DIAMETER = 50;
-
-    createCanvas(SIZE, SIZE);
+    createCanvas(601, 301);
     background("black");
 
-    strokeWeight(3);
-    stroke("yellow");
+    fill("aqua");
+    noStroke();
+
+
+    textSize(50);
+    textAlign(CENTER, CENTER);
+    text("Move the mouse to start", 0, 0, width, height);
+
+
+
+}
+function mouseMoved() {
+    resetMatrix();
+    background("black");
+    strokeWeight(2);
+    noStroke();
+    textSize(15);
+    textAlign(CENTER, CENTER);
+    fill("white");
+    let mousexnum = Math.round(mouseX / 601 * 100);
+    text(`$(mouseX) of 601 = $(mousexnum)%`, 70, 295);
+
     noFill();
+    stroke("aqua");
 
-    for (let y = 0; y <= SIZE; y += CIRCLE_DIAMETER) {
+    // <<< Add your code here
+    let Heighline1 = 0;
+    let Heighline2 = 5;
+    for (let i = 0; i < 601; i++) {
+        for (let j = 0; j < 27; j += 1) {
+            line(0, Heighline1, 10, Heighline2);
+            Heighline1 += 10;
+            line(10, Heighline2, 0, Heighline1);
+            Heighline2 += 10;
 
-        // Save the current origin (left-most position in the current row)
-        push();
-
-        for (let x = 0; x <= SIZE; x += CIRCLE_DIAMETER) {
-            circle(0, 0, CIRCLE_DIAMETER);
-            translate(CIRCLE_DIAMETER, 0); // Move origin to the right
 
         }
-
-        // Restore the stored origin -> back to left-most position in the current row
-        pop();
-
-        // Move one row down
-        translate(0, CIRCLE_DIAMETER);
-
+        translate(10, 0);
+        Heighline1 = 0;
+        Heighline2 = 5;
     }
+
 }
+
+
