9dbb4b65e5b9a5dfe2e91759da5a71ca598b9e90
Rainer Stropek <rainer@timecockpit.com>
2025-02-06 08:28:28 +0100

    Saved in TypeScript Playground at 2025-02-06T07:28:28.397Z

--- bubbles_with_functions/index.ts
+++ bubbles_with_functions/index.ts
@@ -1,46 +1,83 @@
 const circles_x: number[] = [];
 const circles_y: number[] = [];
 const circles_diameter: number[] = [];
- 
+
 let waiting_time = 3000;
 let circle_interval: number;
 let level_interval: number;
- 
+
 let points = 0;
- 
- 
+
+
 function setup() {
   createCanvas(300, 300);
   background("black");
   strokeWeight(2);
   stroke("lime");
   noFill();
- 
- addRandomCircle();
+
+  addRandomCircle();
   circle_interval = setInterval(addRandomCircle, 3000);
- 
-  // <<< Add code here
+
+
 }
- 
+
+
 function draw() {
   background("black");
   if (circles_x.length >= 10) {
-   
+    stopGame();
+
+    return;
   }
- 
-  // <<< Add code here
-  for(let i = 0; i < circles_x.length; i ++){
+
+  strokeWeight(2);
+  stroke("lime");
+  fill("black");
+
+  for (let i = 0; i < circles_x.length; i++) {
     circle(circles_x[i], circles_y[i], circles_diameter[i]);
   }
-}
- 
-// <<< Add functions here
-function addRandomCircle(){
+
+
+  noStroke();
+  fill("yellow");
+  textSize(20);
+  text(`Points: ${points}`, 10, 30);
+} 
+
+
+function addRandomCircle() {
   circles_x.push(random(0, width));
   circles_y.push(random(0, height));
   circles_diameter.push(random(10, 51));
 }
- 
-function isInside(){
- 
+
+function isInside(x: number, y: number, circle_index: number): boolean {
+  const dx = x - circles_x[circle_index];
+  const dy = y - circles_y[circle_index];
+  const distance = Math.sqrt(dx * dx + dy * dy);
+  return distance < circles_diameter[circle_index] / 2;
 }
+
+function stopGame() {
+  clearInterval(circle_interval);
+  clearInterval(level_interval);
+
+  background("black");
+  textSize(30);
+  fill("red");
+  textAlign(CENTER, CENTER);
+  text(`Game Over\Points: ${points}`, 0, 0, width, height);
+  noLoop();
+}
+function mouseClicked() {
+  for (let i = 0; i < circles_x.length; i++) {
+    if (isInside(mouseX, mouseY, i)) {
+      circles_x.splice(i, 1);
+      circles_y.splice(i, 1);
+      circles_diameter.splice(i, 1);
+      points++;
+    }
+  }
+} 
