9a6acec2948db658817526dc5bfec4f2b7179bbc
Rainer Stropek <rainer@timecockpit.com>
2025-04-02 09:57:41 +0000

    Saved in TypeScript Playground at 2025-04-02T09:57:40.795Z

--- joystick_plane/index.ts
+++ joystick_plane/index.ts
@@ -6,14 +6,6 @@ const fighterDisplayHeight = 200;
 const fighterDisplayWidth = fighterDisplayHeight * (fighterImageWidth / fighterImageHeight);
 let fighterPositionX = 0;
 let fighterPositionY = 0;
-let pressedcircle = false;
-
-let smallcircleradius = 10;
-let bigcircleradius = 50;
-
-
-let circleX = 500 / 2;
-let circleY = 500 - 50;
 
 function preload() {
   fighter = loadImage("https://cddataexchange.blob.core.windows.net/images/Spaceship.png");
@@ -25,70 +17,19 @@ function setup() {
 
 function draw() {
   background("lightblue");
-  stroke("red");
-  fill("white");
-  circle(width / 2, height - 50, bigcircleradius * 2);
-
-  noStroke();
-  fill("black");
-  circle(circleX, circleY, smallcircleradius * 2)
 
   push();
   imageMode(CENTER);
   translate(width / 2 + fighterPositionX, height / 2 + fighterPositionY);
   image(fighter, 0, 0, fighterDisplayWidth, fighterDisplayHeight);
   pop();
-   
-   movePlane(circleX, circleY, mouseX, mouseY)
-
-   
 }
-
 
 function mousePressed() {
-  pressedcircle = distance(circleX, circleY);
 }
 
-
 function mouseDragged() {
-  if (pressedcircle === true) {
-    circleX = mouseX
-    circleY = mouseY
-  }
 }
 
 function mouseReleased() {
-  pressedcircle = false;
-}
-
-function distance(x: number, y: number,): boolean {
-  const dx = mouseX - x;
-  const dy = mouseY - y;
-  const distance = Math.sqrt(dx * dx + dy * dy);
-  return distance <= smallcircleradius;
-}
-
-function movePlane(x: number, y: number, xm: number, ym: number) {
-
-  let dx = 0
-  let dy = 0
-
-  dx = x - width / 2;
-  dy = y - (height - 50);
-
-
-  fighterPositionX += dx / 5
-  fighterPositionY += dy / 5
-  if (fighterPositionX > 250) {
-    fighterPositionX = 250
-  }
-  else if (fighterPositionX < -250) {
-    fighterPositionX = -250
-  }
-  if (fighterPositionY > 250) {
-    fighterPositionY = 250
-  }
-  else if (fighterPositionY < -250) {
-    fighterPositionY = -250
-  }
 }
